847f233cd0a0d348c18483cb89e37626409b6825
OSN-DEV <osndev1973@gmail.com>
2018-10-28 08:31:51 +0900

    first commit

---
+++ src/MyRegularExpressionMainFrm.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace MyRegularExpressionTest {
+    partial class MyRegularExpressionMainFrm {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MyRegularExpressionMainFrm));
+            this.cExpressionBackground = new MyLib.ComponentEx.PannelEx(this.components);
+            this.cExpression = new MyLib.ComponentEx.TextBoxEx(this.components);
+            this.cTargetBackground = new MyLib.ComponentEx.PannelEx(this.components);
+            this.cTarget = new MyLib.ComponentEx.RichTextBoxEx(this.components);
+            this.cResultBackground = new MyLib.ComponentEx.PannelEx(this.components);
+            this.cResult = new MyLib.ComponentEx.RichTextBoxEx(this.components);
+            this.cGroupBackground = new MyLib.ComponentEx.PannelEx(this.components);
+            this.cGroup = new MyLib.ComponentEx.TextBoxEx(this.components);
+            this.cExpressionBackground.SuspendLayout();
+            this.cTargetBackground.SuspendLayout();
+            this.cResultBackground.SuspendLayout();
+            this.cGroupBackground.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // cExpressionBackground
+            // 
+            this.cExpressionBackground.BackColor = System.Drawing.Color.Gray;
+            this.cExpressionBackground.Controls.Add(this.cExpression);
+            this.cExpressionBackground.Location = new System.Drawing.Point(10, 10);
+            this.cExpressionBackground.Name = "cExpressionBackground";
+            this.cExpressionBackground.Padding = new System.Windows.Forms.Padding(5, 3, 3, 3);
+            this.cExpressionBackground.Size = new System.Drawing.Size(306, 23);
+            this.cExpressionBackground.TabIndex = 0;
+            // 
+            // cExpression
+            // 
+            this.cExpression.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.cExpression.Font = new System.Drawing.Font("Meiryo UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.cExpression.Location = new System.Drawing.Point(3, 3);
+            this.cExpression.Margin = new System.Windows.Forms.Padding(4);
+            this.cExpression.Name = "cExpression";
+            this.cExpression.Size = new System.Drawing.Size(300, 17);
+            this.cExpression.TabIndex = 0;
+            this.cExpression.TextChanged += new System.EventHandler(this.cExpression_TextChanged);
+            // 
+            // cTargetBackground
+            // 
+            this.cTargetBackground.BackColor = System.Drawing.Color.Gray;
+            this.cTargetBackground.Controls.Add(this.cTarget);
+            this.cTargetBackground.Location = new System.Drawing.Point(10, 76);
+            this.cTargetBackground.Name = "cTargetBackground";
+            this.cTargetBackground.Size = new System.Drawing.Size(306, 106);
+            this.cTargetBackground.TabIndex = 2;
+            // 
+            // cTarget
+            // 
+            this.cTarget.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.cTarget.Font = new System.Drawing.Font("Meiryo UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.cTarget.Location = new System.Drawing.Point(3, 3);
+            this.cTarget.Margin = new System.Windows.Forms.Padding(4);
+            this.cTarget.Name = "cTarget";
+            this.cTarget.Size = new System.Drawing.Size(300, 100);
+            this.cTarget.TabIndex = 2;
+            this.cTarget.Text = "";
+            this.cTarget.TextChanged += new System.EventHandler(this.cTarget_TextChanged);
+            // 
+            // cResultBackground
+            // 
+            this.cResultBackground.BackColor = System.Drawing.Color.Gray;
+            this.cResultBackground.Controls.Add(this.cResult);
+            this.cResultBackground.Location = new System.Drawing.Point(10, 192);
+            this.cResultBackground.Name = "cResultBackground";
+            this.cResultBackground.Size = new System.Drawing.Size(306, 106);
+            this.cResultBackground.TabIndex = 3;
+            // 
+            // cResult
+            // 
+            this.cResult.BackColor = System.Drawing.Color.White;
+            this.cResult.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.cResult.Font = new System.Drawing.Font("Meiryo UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.cResult.Location = new System.Drawing.Point(3, 3);
+            this.cResult.Margin = new System.Windows.Forms.Padding(4);
+            this.cResult.Name = "cResult";
+            this.cResult.ReadOnly = true;
+            this.cResult.Size = new System.Drawing.Size(300, 100);
+            this.cResult.TabIndex = 0;
+            this.cResult.TabStop = false;
+            this.cResult.Text = "";
+            // 
+            // cGroupBackground
+            // 
+            this.cGroupBackground.BackColor = System.Drawing.Color.Gray;
+            this.cGroupBackground.Controls.Add(this.cGroup);
+            this.cGroupBackground.Location = new System.Drawing.Point(10, 43);
+            this.cGroupBackground.Name = "cGroupBackground";
+            this.cGroupBackground.Padding = new System.Windows.Forms.Padding(5, 3, 3, 3);
+            this.cGroupBackground.Size = new System.Drawing.Size(306, 23);
+            this.cGroupBackground.TabIndex = 1;
+            // 
+            // cGroup
+            // 
+            this.cGroup.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.cGroup.Font = new System.Drawing.Font("Meiryo UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.cGroup.Location = new System.Drawing.Point(3, 3);
+            this.cGroup.Margin = new System.Windows.Forms.Padding(4);
+            this.cGroup.Name = "cGroup";
+            this.cGroup.Size = new System.Drawing.Size(300, 17);
+            this.cGroup.TabIndex = 1;
+            this.cGroup.TextChanged += new System.EventHandler(this.cGroup_TextChanged);
+            // 
+            // MyRegularExpressionMainFrm
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(326, 310);
+            this.Controls.Add(this.cGroupBackground);
+            this.Controls.Add(this.cResultBackground);
+            this.Controls.Add(this.cTargetBackground);
+            this.Controls.Add(this.cExpressionBackground);
+            this.Font = new System.Drawing.Font("Meiryo UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MinimumSize = new System.Drawing.Size(342, 317);
+            this.Name = "MyRegularExpressionMainFrm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "正規表現確認";
+            this.Load += new System.EventHandler(this.MyRegularExpressionMainFrm_Load);
+            this.Resize += new System.EventHandler(this.MyRegularExpressionMainFrm_Resize);
+            this.cExpressionBackground.ResumeLayout(false);
+            this.cExpressionBackground.PerformLayout();
+            this.cTargetBackground.ResumeLayout(false);
+            this.cResultBackground.ResumeLayout(false);
+            this.cGroupBackground.ResumeLayout(false);
+            this.cGroupBackground.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private MyLib.ComponentEx.PannelEx cExpressionBackground;
+        private MyLib.ComponentEx.TextBoxEx cExpression;
+        private MyLib.ComponentEx.PannelEx cTargetBackground;
+        private MyLib.ComponentEx.RichTextBoxEx cTarget;
+        private MyLib.ComponentEx.PannelEx cResultBackground;
+        private MyLib.ComponentEx.RichTextBoxEx cResult;
+        private MyLib.ComponentEx.PannelEx cGroupBackground;
+        private MyLib.ComponentEx.TextBoxEx cGroup;
+    }
+}
---
+++ src/MyRegularExpressionMainFrm.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using MyLib.ComponentEx;
+
+namespace MyRegularExpressionTest {
+    public partial class MyRegularExpressionMainFrm : FormEx {
+
+        #region Declaration
+        private class Cst {
+            public const int Padding = 3;
+            public const int Margin = 10;
+            public const int Offset = 12;
+        }
+        #endregion
+
+        #region Constructor
+        public MyRegularExpressionMainFrm() {
+            InitializeComponent();
+        }
+        #endregion
+
+        #region Form Event
+        private void MyRegularExpressionMainFrm_Load(object sender, EventArgs e) {
+            System.Reflection.Assembly assenbley = System.Reflection.Assembly.GetExecutingAssembly();
+            //バージョンの取得
+            this.Text = "正規表現確認(" + assenbley.GetName().Version + ")";
+            this.adjustControl();
+
+            string pattern = @"(\[car:)(?<carName>.+?)(\])";
+            Regex reg = new Regex(pattern);
+            string target = "[car:honda][car:atenza]";
+            var match = reg.Matches(target);
+            foreach (Match m in match) {
+                string carName = m.Groups["carName"].Value;
+            }
+
+        }
+        private void MyRegularExpressionMainFrm_Resize(object sender, EventArgs e) {
+            this.adjustControl();
+        }
+        #endregion
+
+        #region Control Event
+        private void cExpression_TextChanged(object sender, EventArgs e) {
+            this.showResult();
+        }
+
+        private void cTarget_TextChanged(object sender, EventArgs e) {
+            this.cResult.Text = this.cTarget.Text;
+            this.showResult();
+        }
+
+        private void cGroup_TextChanged(object sender, EventArgs e) {
+            this.showResult();
+        }
+        #endregion
+
+        #region Private Method
+        /// <summary>
+        /// adjust constrols for form
+        /// </summary>
+        private void adjustControl() {
+            // マジックナンバーは実動作をみて調整した値(数値の根拠は不明)
+
+            this.cExpressionBackground.Width = this.Width - (Cst.Offset * 2) - Cst.Offset;
+            this.cExpression.Width = this.cExpressionBackground.Width - (Cst.Padding * 2);
+
+            this.cGroupBackground.Width = this.cExpressionBackground.Width;
+            this.cGroup.Width = this.cExpression.Width;
+
+            this.cTargetBackground.Width = this.cExpressionBackground.Width;
+            this.cTarget.Width = this.cExpression.Width;
+            this.cResultBackground.Width = this.cExpressionBackground.Width;
+            this.cResult.Width = this.cExpression.Width;
+
+            int areaHeight = this.Height - this.cTargetBackground.Top - Cst.Margin - Cst.Margin - 40;
+            int boxHeight = (int)(areaHeight / 2);
+
+            this.cTargetBackground.Height = boxHeight;
+            this.cTarget.Height = this.cTargetBackground.Height - (Cst.Padding * 2);
+            this.cResultBackground.Height = this.cTargetBackground.Height;
+            this.cResult.Height = this.cTarget.Height;
+
+            this.cResultBackground.Top = this.Height - this.cResultBackground.Height - Cst.Margin - SystemInformation.CaptionHeight - 18;
+        }
+
+        /// <summary>
+        /// show result
+        /// </summary>
+        private void showResult() {
+            this.cResult.Select(0, this.cResult.Text.Length);
+            this.cResult.SelectionBackColor = Color.White;
+            this.cResult.SelectionColor = Color.Black;
+
+            if (0 == this.cTarget.Text.Length) {
+                return;
+            }
+            if (0 == this.cExpression.Text.Length) {
+                return;
+            }
+
+            // https://msdn.microsoft.com/ja-jp/library/cc720850.aspx
+            try {
+                Regex reg = new Regex(this.cExpression.Text);
+
+                string[] text = this.cTarget.Text.Split('\n');
+                int readSize = 0;
+                for (int i = 0; i < text.Length; i++) {
+                    MatchCollection matches = reg.Matches(text[i]);
+                    int index = 0;
+
+                    foreach (Match match in matches) {
+                        this.cResult.Select(readSize + match.Index, match.Length);
+                        if (index % 2 == 0) {
+                            this.cResult.SelectionBackColor = Color.LightPink;
+                        }else {
+                            this.cResult.SelectionBackColor = Color.LightGreen;
+                        }
+
+
+                        if (0 < this.cGroup.Text.Length) {
+                            string groupVal = match.Groups[this.cGroup.Text].Value;
+                            if (null != groupVal && 0 < groupVal.Length) {
+                                int pos = this.cResult.SelectedText.IndexOf(groupVal);
+                                this.cResult.Select(readSize + match.Index + pos, groupVal.Length);
+                                this.cResult.SelectionColor = Color.Red;
+                            }
+                        }
+                        index++;
+
+                    }
+                    readSize += text[i].Length + 1;
+                }
+                this.cResult.Select(0, 0);
+            } catch (Exception ex) {
+                // エスケープシーケンスの\の入力中等、不正な文字列としてエラーではじかれることがあるので例外は無視
+            }
+        }
+        #endregion
+
+    }
+}
